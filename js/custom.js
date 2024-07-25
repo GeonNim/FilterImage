b85ac25fddc29d38da19d0301b769d0e4995d9df
GeoNnim <yy4338@gamil.com>
2024-07-25 18:50:55 +0900

    dd

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,27 @@
 //Create button element
 
-const btns = ['all', 'bag', 'shoe', 'watch', 'camera', 'headphone'];
+const btnChars = ['all', 'bag', 'shoe', 'watch', 'camera', 'headphone'];
 const btnsWrapper = document.querySelector('.filter-btns');
 
-btns.map(function (btn) {
-  const modifiedChar = btn.charAt(0).toUpperCase() + btn.slice(1);
-  // str.charAt(): 문자열의 첫 번째 글자만 반환 (*괄호 안 인덱스 번호에 따라 달라짐)
-  // str.toUpperCase(): 문자열을 대문자로 변환
-  // str.toLowerCase(): 문자열을 소문자로 변환
-  // str.slice(): 문자열의 해당 인덱스부터 끝까지 반환
+btnChars.map(function (btnChar) {
+  const modifiedChar = btnChar.charAt(0).toUpperCase() + btnChar.slice(1);
+  // charAt(): 문자열의 첫 번째 글자만 반환 (*괄호 안 인덱스 번호에 따라 달라짐)
+  // toUpperCase(): 문자열을 대문자로 변환
+  // toLowerCase(): 문자열을 소문자로 변환
+  // slice(): 문자열의 해당 인덱스부터 끝까지 반환
 
   const btnElement = `
-  <button class="filter-btn" data-filter="${btn}">${modifiedChar}</button>
+  <button class="filter-btn" data-filter="${btnChar}">${modifiedChar}</button>
   `;
   btnsWrapper.insertAdjacentHTML('beforeend', btnElement);
 });
 
-const imgs = [
+//Firtst button add actiove class
+const btns = document.querySelectorAll('.filter-btn');
+btns[0].classList.add('active');
+
+// Create images element
+const images = [
   'bag-1.jpg',
   'camera-1.jpg',
   'camera-2.jpg',
@@ -26,15 +31,74 @@ const imgs = [
   'shoe-2.jpg',
   'watch-1.jpg',
 ];
-const imgsWrapper = document.querySelector('.filter-images');
-imgs.map(function (img) {
+const imagesWrapper = document.querySelector('.filter-images');
+
+images.map(function (image) {
   // console.log(img.split('-')[0]);
-  console.log(img.indexOf('/'));
+
   const imgElement = `
-     <div class="filter-image" data-filter="${img.split('-')[0]}">
-        <span><img src="images/${img}" alt="" /></span>
+     <div class="filter-image" data-filter="${image.split('-')[0]}">
+        <span><img src="images/${image}" alt="" /></span>
      </div>
   `;
-  // console.log(imgElement);
-  imgsWrapper.insertAdjacentHTML('beforeend', imgElement);
+
+  imagesWrapper.insertAdjacentHTML('beforeend', imgElement);
+});
+
+const imageElements = document.querySelectorAll('.filter-image');
+
+//Filter Images
+function activateFilter() {
+  btns.forEach((btn) => {
+    btn.classList.remove('active');
+  });
+  this.classList.add('active');
+
+  const selectedBtn = this.getAttribute('data-filter');
+
+  // map, filter, reduce 함수는 DOM 요소에 사용할 수 없다.
+  // Array.from()을 사용하여 배열로 변환한다.
+  Array.from(imageElements).filter((imageElement) => {
+    imageElement.classList.add('hide');
+    imageElement.classList.remove('show');
+
+    setTimeout(() => {
+      if (
+        imageElement.getAttribute('data-filter') === selectedBtn ||
+        selectedBtn === 'all'
+      ) {
+        imageElement.classList.remove('hide');
+        imageElement.classList.add('show');
+      } else {
+        imageElement.classList.remove('show');
+        imageElement.classList.add('hide');
+      }
+    }, 100);
+    //시간 지연 함수(promise) : 첫번째 파라미터 = callback function, 두번째 파라미터 = 시간(밀리초)
+  });
+}
+
+btns.forEach(function (btn) {
+  btn.addEventListener('click', activateFilter);
+});
+
+// activate light box when click each image
+const showLightBox = (e) => {
+  const taget = e.currentTarget;
+  const selectedImage = taget.children[0].children[0].getAttribute('src');
+  const categoryName = taget.getAttribute('data-filter');
+
+  const lightBoxImage = document.querySelector('.light-box-image img');
+  const categoryElement = document.querySelector('.title p');
+
+  // getAttribute(): 파라미터 속성 값 가져오기
+  // setAttribute(a, b): a: 속성 이름, b: 변경할 속성 값
+  // a.textContent = b: a 요소에 b 텍스트 입력
+
+  lightBoxImage.setAttribute('src', selectedImage);
+  categoryElement.textContent = categoryName;
+};
+
+imageElements.forEach((imageElement) => {
+  imageElement.addEventListener('click', showLightBox);
 });
